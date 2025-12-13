9d876cd3252bb6ad160736561c2ad4bfcd187ecd
Carvajalandre <andrexsantiago20@gmail.com>
2025-12-13 17:15:33 -0500

    interaction table add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             TicketStatusSeeder::class,
             TicketSeeder::class,
+            InteractionSeeder::class,
         ]);
     }
 }
